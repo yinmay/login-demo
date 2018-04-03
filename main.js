010b656e848365640acf8e6ff8544bffaa79353d
yinmei <1102316727@qq.com>
2018-04-03 08:59:26 +0800

    login

--- main.js
+++ main.js
@@ -1,13 +1,50 @@
 let $signUpForm = $('form[name=signUp]')
-$signUpForm .on('submit', (e)=>{
+let $loginForm = $('form[name=login]')
+$loginForm.on('submit', (e)=>{
     e.preventDefault()
+    let string = $loginForm . serialize()
+    //表单验证...
+    $.ajax({
+        url:$loginForm.attr('action'),
+        method:$loginForm.attr('method'),
+        success:function(){
+            location.href = '/home'
+        },
+        error:function(){}
+    })  
+})
+
+$signUpForm.on('submit', (e)=>{
+    e.preventDefault()
+    //get data
     let string = $signUpForm . serialize()
+    //check form
+    let errors = checkForm($signUpForm)
+    if(Object.keys(errors).length !== 0){
+        showErrors(errors,$signUpForm)
+     }else{
+        $.ajax({
+            url:$signUpForm.attr('action'),
+            method:$signUpForm.attr('method'),
+            data:string,
+            success:function(response){
+                location.href = '/home'
+            },
+            error: function(xhr){
+                let errors = JSON.parse(xhr.responseText)
+                showErrors(errors,$signUpForm)
+            }
+        })
+     }
+})
+
+function checkForm($signUpForm){
     //check form
     let email = $signUpForm.find('[name=email]').val()
     let password = $signUpForm.find('[name=password]').val()
     let password_confirmation = $signUpForm.find('[name=password_confirmation]').val()
     let errors = {}
-      //check email,password
+     // check email,password
       if(email.indexOf('@')<=0){
         errors.email = '邮箱不合法'
       }
@@ -17,25 +54,16 @@ $signUpForm .on('submit', (e)=>{
       if(password_confirmation !== password){
         errors.password_confirmation = '两次输入密码不匹配'
       }
-      //clear errors
-      $signUpForm.find(`span[name$=_error]`).each(function(index,span){
-         $(span).text('')
-      })
-     if(Object.keys(errors).length !== 0){
-         for(var key in errors){
-             let value = errors[key]
-             $signUpForm.find(`[name=${key}_error]`).text(value)
-         }
-         return
-     }
-
-    $.ajax({
-        url:$signUpForm.attr('action'),
-        method:$signUpForm.attr('method'),
-        data:string,
-        success:function(response){
-            let object = JSON.parse(response)
-            console.log(object)
-        }
+      return errors
+}
+function showErrors(errors,$signUpForm){
+    //clear errors
+    $signUpForm.find(`span[name$=_error]`).each(function(index,span){
+    $(span).text('')
     })
-})
+    //show errors
+    for(var key in errors){
+        let value = errors[key]
+        $signUpForm.find(`[name=${key}_error]`).text(value)
+    }
+}
--- server.js
+++ server.js
@@ -17,32 +17,32 @@ var server = http.createServer(function(request, response){
   var query = parsedUrl.query
   var method = request.method
 
-  /******** 从这里开始看，上面不要看 ************/
-
-//   console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
-
   if(path === '/'){
       var string = fs.readFileSync('./index.html')
-    // response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    // response.write('哈哈哈')
     response.end(string)
   }else if(path == "/signUp" && method === "POST"){
+    // 解析postData
     getPostData(request,function(postData){
-      let {email,password,password_confirmation} = postData
-   
-      // console.log(email,password,password_confirmation)
-      let errors = {}
-      //check email,password
-      if(email.indexOf('@')<=0){
-        errors.email = '邮箱不合法'
-      }
-      if(password.length<6){
-        errors.password = '密码太短'
-      }
-      if(password_confirmation !== password){
-        errors.password_confirmation = '两次输入密码不匹配'
-      }
+    //检查postData，保存在errors上面
+      let errors = checkPostData(postData)
+      if(Object.keys(errors).length === 0){
+        let {email,password} = postData
+        let user = {
+          email:email,
+          passwordHash:meiHash(password),
+        }
+
+        // 写数据库
+        let dbString = fs.readFileSync('./db.json','utf-8')
+        console.log(dbString)
+        let dbObject = JSON.parse(dbString)
+        dbObject.users.push(user)
+        let dbString2 = JSON.stringify(dbObject)
+        fs.writeFileSync('./db.json',dbString2,{encoding:'utf-8'}) 
+    }else {
+      response.statusCode = 400
+    }
       response.setHeader('Content-Type', 'text/html;charset=utf-8')
       response.end(JSON.stringify(errors))
 
@@ -55,6 +55,40 @@ var server = http.createServer(function(request, response){
     let string = fs.readFileSync('./main.js')
     response.setHeader('Content-Type', 'application/javascript;charset=utf-8')
     response.end(string)
+  }else if(path === '/home'){
+    var cookies = parseCookies(request.headers.cookie)
+    if(cookies.logined === 'true'){
+      response.end('已登录')
+    }else{
+      let string = fs.readFileSync('./home.html')
+      response.setHeader('Content-Type', 'text/html;charset=utf-8')
+      response.end(string)
+    }
+  }else if(path === '/login' && method === 'POST'){
+    //读数据库
+    getPostData(request,(postData)=>{
+      console.log(postData)
+      let dbString = fs.readFileSync('./db.json','utf-8')
+      let dbObject = JSON.parse(dbString)
+      let users = dbObject.users
+
+      let{email,password}= postData
+      let found
+      // console.log(email,password)
+      //查看用户输入的与数据库里面的是否一致
+      for(var i=0;i<users.length;i++){
+        if(users[i].email === email && users[i].passwordHash === meiHash(password)){
+          found = users[i]
+          break
+        }
+      }
+      if(found){
+        //标记该用户登录了
+        response.setHeader('Set-Cookie',stringifyCookies({logined:true}))
+      }
+      let errors = {}
+      response.end(JSON.stringify(errors))
+    })
   }
   else{
     response.statusCode = 404
@@ -74,7 +108,6 @@ function getPostData(request,callback){
     data += postData.toString()
   })
   request.on('end', ()=>{
-   
     let array = data.split('&')
     let postData = {}
     for(var i=0;i<array.length;i++){
@@ -87,6 +120,51 @@ function getPostData(request,callback){
         callback.call(null,postData)
   })
   
+}
+
+function checkPostData(postData){
+  let {email,password,password_confirmation} = postData
+  let errors = {}
+  if(email.indexOf('@')<=0){
+    errors.email = '邮箱不合法'
+  }
+  if(password.length<6){
+    errors.password = '密码太短'
+  }
+  if(password_confirmation !== password){
+    errors.password_confirmation = '两次输入密码不匹配'
+    }
+  return errors
+}
+
+//加密
+function meiHash(string){
+  return string+'mei'
+}
+
+function parseCookies(cookie) {
+  try{
+  return cookie.split(';').reduce(
+      function(prev, curr) {
+          var m = / *([^=]+)=(.*)/.exec(curr);
+          var key = m[1];
+          var value = decodeURIComponent(m[2]);
+          prev[key] = value;
+          return prev;
+      },
+      { }
+  );
+  }catch(error){
+    return {}
+}
+}
+
+function stringifyCookies(cookies) {
+  var list = [ ];
+  for (var key in cookies) {
+      list.push(key + '=' + encodeURIComponent(cookies[key]));
+  }
+  return list.join('; ');
 }
 
 server.listen(port)
